e46a90bb602ab41060c55eb53b4141f39bc166cf
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-12-29 16:24:21 +0700

    feat: route for checkout midtrans

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Proses Checkout
+Route::post('/checkout', [ShopController::class, 'checkout']);
+
+// Proses Pembayarab + Notif
+Route::post('/payment/callback', [ShopController::class, 'callback']);
